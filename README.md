2947d15c373dcf3a13830221766c14a55cab2943
haoda <>
2020-10-09 17:08:57 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## how to use
+
+### 1. install node modules
+RUN: `yarn install` or `npm install`
+
+### 2. add config file
+* add `local.json` at `./config`. *This file has been ruled by .gitignore*
+* copy all content from `./config/default.json`
+
+
+### 3. override config *(ask admin to provide following information)* 
+* server host port
+* padlocal token
+* server ca certification 
+
+### 4. try the demo
+RUN: `yarn run demo` or `npm run demo`
---
+++ main.ts
@@ -0,0 +1,87 @@
+import {PuppetPadlocal} from "wechaty-puppet-padlocal";
+import {Contact, Friendship, Message, Room, RoomInvitation, ScanStatus, Wechaty} from "wechaty";
+import * as config from "config";
+require('console-stamp')(console);
+
+const host: string = config.get("padLocal.host");
+const port: number = config.get("padLocal.port");
+const token: string = config.get("padLocal.token");
+const serverCAFilePath: string = config.get("padLocal.serverCAFilePath");
+
+const puppet = new PuppetPadlocal({
+    endpoint: `${host}:${port}`,
+    token,
+    serverCAFilePath
+})
+
+const bot =  new Wechaty({
+    name: "TestBot",
+    puppet,
+});
+
+type PrepareBotFunc = (bot: Wechaty) => Promise<void>;
+
+async function prepareSingedOnBot(prepareBotFunc?: PrepareBotFunc): Promise<Wechaty> {
+    bot.on("scan", (qrcode: string, status: ScanStatus) => {
+        if (status === ScanStatus.Waiting && qrcode) {
+            const qrcodeImageUrl = ["https://api.qrserver.com/v1/create-qr-code/?data=", encodeURIComponent(qrcode)].join("");
+            console.log("TestBot", `onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
+        } else {
+            console.log("TestBot", `onScan: ${ScanStatus[status]}(${status})`);
+        }
+    });
+
+    bot.on("login", (user: Contact) => {
+        console.log("TestBot", `${user} login`);
+    });
+
+    bot.on("logout", (user: Contact) => {
+        console.log("TestBot", `${user} logout`);
+    });
+
+    await bot.start();
+
+    console.log("TestBot", "TestBot started.");
+
+    await prepareBotFunc?.(bot);
+
+    await bot.ready();
+
+    console.log("TestBot", "TestBot ready.");
+
+    return bot;
+}
+
+async function main() {
+    await prepareSingedOnBot(async (bot) => {
+        bot.on("message", async (message: Message) => {
+            console.log(`on message: ${message.toString()}`);
+        });
+
+        bot.on("friendship", async (friendship: Friendship) => {
+            console.log(`on friendship: ${friendship.toJSON()}`);
+        });
+
+        bot.on("room-invite", async (roomInvite: RoomInvitation) => {
+            console.log(`on room invite: ${await roomInvite.toJSON()}`);
+        });
+
+        bot.on("room-join", async (room: Room, inviteeList: Contact[], inviter: Contact, date) => {
+            console.log(
+                `on room join: ${room.toString()}, ${inviteeList.map((i) => i.toString())}, ${inviter.toString()}, ${date}`
+            );
+        });
+
+        bot.on("room-leave", async (room: Room, leaverList: Contact[], remover?: Contact, date?: Date) => {
+            console.log(
+                `on room leave: ${room.toString()}, ${leaverList.map((l) => l.toString())}, ${remover?.toString()} ${date}`
+            );
+        });
+
+        bot.on("room-topic", async (room: Room, newTopic: string, oldTopic: string, changer: Contact, date?: Date) => {
+            console.log(`on room topic: ${room.toString()}, ${newTopic}, ${oldTopic}, ${changer.toString()}, ${date}`);
+        });
+    });
+}
+
+main().then()
