18542ea4e24627878030af2b14f400eb7fcafcc2
padlocal <oxddoxdd@gmail.com>
2021-01-05 14:11:33 +0800

    update wechaty-puppet-padlocal version to 0.2.16

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import {PuppetPadlocal} from "wechaty-puppet-padlocal";
-import {Contact, Message, ScanStatus, Wechaty} from "wechaty";
-require('console-stamp')(console);
+import {Contact, log, Message, ScanStatus, Wechaty} from "wechaty";
 
 const puppet = new PuppetPadlocal({
     token: "YOUR_PADLOCAL_TOKEN"
@@ -14,26 +13,30 @@ const bot = new Wechaty({
 .on("scan", (qrcode: string, status: ScanStatus) => {
     if (status === ScanStatus.Waiting && qrcode) {
         const qrcodeImageUrl = ["https://api.qrserver.com/v1/create-qr-code/?data=", encodeURIComponent(qrcode)].join("");
-        console.log("TestBot", `onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
+        log.info("TestBot", `onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
     } else {
-        console.log("TestBot", `onScan: ${ScanStatus[status]}(${status})`);
+        log.info("TestBot", `onScan: ${ScanStatus[status]}(${status})`);
     }
 })
 
 .on("login", (user: Contact) => {
-    console.log("TestBot", `${user} login`);
+    log.info("TestBot", `${user} login`);
 })
 
 .on("logout", (user: Contact, reason: string) => {
-    console.log("TestBot", `${user} logout, reason: ${reason}`);
+    log.info("TestBot", `${user} logout, reason: ${reason}`);
 })
 
 .on("message", async (message: Message) => {
-    console.log("TestBot", `on message: ${message.toString()}`);
+    log.info("TestBot", `on message: ${message.toString()}`);
+})
+
+.on("error", (error) => {
+    log.error("TestBot", 'on error: ', error.stack);
 })
 
 
 bot.start().then(() => {
-    console.log("TestBot", "started.");
+    log.info("TestBot", "started.");
 });
 
