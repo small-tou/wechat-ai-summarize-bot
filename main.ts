6cc68c3c5fbf7bfa2c02dcdac181ab4f0c695af2
padlocal <oxddoxdd@gmail.com>
2021-02-18 12:23:25 +0800

    fix bug #6

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import {PuppetPadlocal} from "wechaty-puppet-padlocal";
-import {Contact, log, Message, ScanStatus, Wechaty} from "wechaty";
+import { PuppetPadlocal } from "wechaty-puppet-padlocal";
+import { Contact, log, Message, ScanStatus, Wechaty } from "wechaty";
 
 const puppet = new PuppetPadlocal({
     token: "YOUR_PADLOCAL_TOKEN"
@@ -10,30 +10,36 @@ const bot = new Wechaty({
     puppet,
 })
 
-.on("scan", (qrcode: string, status: ScanStatus) => {
-    if (status === ScanStatus.Waiting && qrcode) {
-        const qrcodeImageUrl = ["https://api.qrserver.com/v1/create-qr-code/?data=", encodeURIComponent(qrcode)].join("");
-        log.info("TestBot", `onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
-    } else {
-        log.info("TestBot", `onScan: ${ScanStatus[status]}(${status})`);
-    }
-})
+    .on("scan", (qrcode: string, status: ScanStatus) => {
+        if (status === ScanStatus.Waiting && qrcode) {
+            const qrcodeImageUrl = [
+                'https://wechaty.js.org/qrcode/',
+                encodeURIComponent(qrcode),
+            ].join('')
 
-.on("login", (user: Contact) => {
-    log.info("TestBot", `${user} login`);
-})
+            log.info("TestBot", `onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
 
-.on("logout", (user: Contact, reason: string) => {
-    log.info("TestBot", `${user} logout, reason: ${reason}`);
-})
+            require('qrcode-terminal').generate(qrcode, { small: true })  // show qrcode on console
+        } else {
+            log.info("TestBot", `onScan: ${ScanStatus[status]}(${status})`);
+        }
+    })
 
-.on("message", async (message: Message) => {
-    log.info("TestBot", `on message: ${message.toString()}`);
-})
+    .on("login", (user: Contact) => {
+        log.info("TestBot", `${user} login`);
+    })
 
-.on("error", (error) => {
-    log.error("TestBot", 'on error: ', error.stack);
-})
+    .on("logout", (user: Contact, reason: string) => {
+        log.info("TestBot", `${user} logout, reason: ${reason}`);
+    })
+
+    .on("message", async (message: Message) => {
+        log.info("TestBot", `on message: ${message.toString()}`);
+    })
+
+    .on("error", (error) => {
+        log.error("TestBot", 'on error: ', error.stack);
+    })
 
 
 bot.start().then(() => {
